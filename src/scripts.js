989b4ce0fa68cf5413f5f9689b4efe58d1bfa610
Johann Patricio Daniel <156346733+JohannPDaniel@users.noreply.github.com>
2024-04-28 23:10:15 -0300

    criando mensagem

--- src/scripts.js
+++ src/scripts.js
@@ -8,7 +8,7 @@ app.use(cors());
 
 app.use(express.json());
 
-const PORT = 8080;
+const PORT = 3000;
 // ------------------------------------------------------------------
 
 /*
@@ -23,7 +23,7 @@ const PORT = 8080;
 let listaMensagem = []
 let idAutomatico = 1
 
-let admins = []
+let users = []
 let idAdmin = 1
 
 // http://localhost:8080
@@ -64,7 +64,7 @@ app.post('/signup', async (request,response) => {
         return response.status(400).json({ Mensagem: "Por favor, verifique se passou o email" })
     }
 
-    const verificarEmail = admins.find((admin)=> admin.email === email)
+    const verificarEmail = users.find((admin)=> admin.email === email)
 
     if(verificarEmail){
         return response.status(400).json({ Mensagem: "Email já cadastrado, insira outro" })
@@ -76,51 +76,90 @@ app.post('/signup', async (request,response) => {
 
     const senhaCriptografada = await bcrypt.hash(password,10)
 
-    let novoAdministrador = {
+    let newUser = {
         id : idAdmin,
+        name: name,
         email : email, 
         password : senhaCriptografada
     }
     
-    admins.push(novoAdministrador)
+    users.push(newUser)
   
     idAdmin++
   
-        return response.status(201).json({ Mensagem: `Pessoa administradora do email ${email}, cadastrada com sucesso!`,
-    })
+        return response.status(201).json({ 
+            Mensagem: `Pessoa administradora do email ${email}, cadastrada com sucesso!`,
+        })
 })
 
 // ------------------ login --------------------
 // http://localhost:8080/login
 
-app.post('/login', async (request,response) => {
-    const { email, password } = request.body
+app.post('/login', async (request, response) => {
+    const { email, password } = request.body;
 
-    if (!email || email.trim() === "") {
-        return response.status(400).json({ Mensagem: "Por favor, verifique se passou o email" })
+    if (!email || email.trim() === '') {
+        return response.status(400).json({ mensagem: 'Insira um e-mail válido' });
     }
 
-    if (!password || password.trim() === "") {
-        return response.status(400).json({ Mensagem: "Por favor, verifique se passou a senha " })
+    if (!password || isNaN(password)) {
+        return response.status(400).json({ mensagem: 'Insira uma senha válida' });
     }
+    const admin = users.find(admin => admin.email === email);
 
-    const admin = admins.find(admin => admin.email === email);
-  
     if (!admin) {
-      return response.status(404).json({ mensagem: 'Admin não encontrado' });
+        return response.status(404).json({ 
+            mensagem: ' Email não encontrado no sistema, verifique ou crie uma conta' 
+        });
     }
-  
+    
     const senhaMatches = await bcrypt.compare(password, admin.password);
-   
-    if (!senhaMatches) {
-      return response.status(400).json({ mensagem: 'Senha incorreta ou credencial inválida' });
-    }
-  
-    return response.status(200).json({
-      mensagem: `Pessoa com email ${email} foi logada com sucesso! Seja bem-vindo!`,
-      email: email,
-      loginStatus: 'Success'
-    });
-  });
 
-app.listen(PORT, () => console.log("Servidor rodando na porta", PORT))
+    if (!senhaMatches) {
+        return response.status(401).json({ mensagem: 'Senha incorreta ou credencial inválida' });
+    }
+
+    return response.status(200).json({
+        mensagem: `Seja bem-vinda(o) ${admin.name}! Pessoa usuária logada com sucesso!`,
+    });
+});
+
+/*
+    
+*/
+// http://localhost:8080/message
+app.post('/message', (request,response) => {
+    const data = request.body
+    const { title, descrition } = data
+
+    if (!title || title.trim() === "") {
+        return response.status(400).json({
+            Mensagem: "Favor enviar um titulo válido"
+        })
+    }
+
+    if (!descrition || descrition.trim() === "") {
+        return response.status(400).json({
+            Mensagem: "Favor enviar uma descrição válida"
+        })
+    }
+
+    let newMessage = {
+        id:idAutomatico,
+        title,
+        descrition
+    }
+
+    users.push(newMessage)
+
+    idAutomatico++
+
+    return response.status(201).json({
+        Mensagem: `Mensagem criada com sucesso`,
+        newMessage
+    })
+})
+
+
+
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
